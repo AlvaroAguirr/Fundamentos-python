922e6b4da0eaed55cd7cc0ece04285929134eee2
alvaro aguirre <alguirre2@gmail.com>
2023-06-08 14:32:59 -0600

    02 variables

---
+++ Variables.py
@@ -0,0 +1,16 @@
+# python es NO tipado
+
+name = "alvaro"
+age = 18
+is_single = True
+print (name)
+print(age)
+print(is_single)
+
+""" 
+hola soy alvaro tengo 18 años y soltero igual a true
+
+"""
+#utilizando format String  
+print("hola soy {}, tengo {} años, y soltero = {}". format(name, age, is_single))
+print(f"hola soy {name}, tengo {age} años, y soltero = {is_single}")
