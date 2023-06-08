e10f1a27ba4f25d1765f33d39d5d660e7f1d3567
alvaro aguirre <alguirre2@gmail.com>
2023-06-08 14:14:34 -0600

    iniciando con python

---
+++ main.py
@@ -0,0 +1,14 @@
+#funcion print
+
+print("este es un string","hola mundo")
+print("este es un entero", 35)
+print("este es es un float", 123.5)
+print( "este es es un boleano",True)
+
+
+#funcion tipe nos permite saber tipos de dato
+
+print(type(True))
+print(type(40))
+print(type(40.1))
+print(type("hola que hace"))
