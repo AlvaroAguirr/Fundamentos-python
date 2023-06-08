bc5bbc138d32d522b06c7a8e7d9dad36ce638dcf
alvaro aguirre <alguirre2@gmail.com>
2023-06-08 14:43:15 -0600

    03 operadores

---
+++ operadores.py
@@ -0,0 +1,19 @@
+# operadores matematicos
+#+-*%
+# / division flotante
+#// division entera
+# ** potencia 
+
+print(24+12)
+print(12+34)
+print( 20*4)
+print(10 %3)
+print(10/3)
+print(10//3)
+print(3**3)
+
+#operadores String 
+#+ para concatenar
+# * replicar un String
+print( "hola a "+ "todos")
+print(" DSM 3A"*4)
