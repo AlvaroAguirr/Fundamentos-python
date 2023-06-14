d14ba49e4bb59274be719b32b1b11ca06eab6b72
alvaro aguirre <alguirre2@gmail.com>
2023-06-14 13:43:43 -0600

    07 Strings

---
+++ Strings.py
@@ -0,0 +1,33 @@
+my_String = "estE es ES un TexTO"
+print(type(my_String))
+
+print(my_String.capitalize())
+print(my_String.upper())
+print(my_String.lower())
+print(my_String.title())
+
+
+print (len(my_String))
+
+#indexing 
+
+my_String="Hola alumnos!"
+
+print(my_String[3])
+
+for letter in my_String:
+    print(f"la letra { letter }")
+
+print(my_String [-1])
+print(my_String [-2])
+
+
+print(my_String[:] )
+print(my_String[0:3] )
+print(my_String[3:] )
+print(my_String[3:7] )
+print(my_String[:7] )
+print(my_String[::-1] )
+
+
+  
