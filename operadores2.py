de0d8c3c83f031748542301848a00bccd6226abd
alvaro aguirre <alguirre2@gmail.com>
2023-06-14 12:35:48 -0600

    05 operadores logicos y booleanos

---
+++ operadores2.py
@@ -0,0 +1,28 @@
+#OPERADORES Boolenaos 
+print("operadores booleanos")
+
+# < <=
+print(10<10)
+print(10<=10)
+# > >=
+print(10>10)
+print(10>=10)
+# ==
+print(10==10)
+# !=
+print(10 !=10)
+
+
+#operadores logicos
+print("operadores logicos")
+# and(true if all is true) 
+print(True and True)
+print(True and False)
+# or (true if at least one true)
+print(False or False)
+print(False or True)
+#not true
+print(not True)
+print(not False)
+
+
