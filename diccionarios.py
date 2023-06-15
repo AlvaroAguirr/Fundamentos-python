b438331345126da33eb4591db8ac92f224ba1296
alvaro aguirre <alguirre2@gmail.com>
2023-06-15 13:39:48 -0600

    10 diccionarios

---
+++ diccionarios.py
@@ -0,0 +1,20 @@
+#diccionarios 
+#{ "key": "value", "key": value }
+
+team = {
+    "name": "City",
+    "country": "England",
+    "champions":1,
+    "players": ['halland','grealinsh']
+}
+
+print(type(team))
+print(team)
+print (team["name"])
+print (team["players"])
+team["players"].append("Kevin")
+team["name"] = "manchertes city"
+team["league"] = "premiere league" 
+print(team)
+
+
